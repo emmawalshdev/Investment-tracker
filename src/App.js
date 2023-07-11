254db587881e8ac5a553925d6db24212d178aa09
Emma Walsh <emmaharte33@outlook.com>
2023-07-11 12:58:25 +0100

    Manage userInput data via state

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
+import React, {useState} from 'react';
 import Header from './components/UI/Header';
 import NewInvestment from './components/NewInvestment//NewInvestment';
-import Investment from './components/InvestmentResults/InvestmentResults';
+import InvestmentResults from './components/InvestmentResults/InvestmentResults';
 
 function App() {
-  const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
 
-    const yearlyData = []; // per-year results
+  const [userInput, setUserInput] = useState(null);
 
+  const calculateHandler = (userInput) => { // receives userinput from NewInvestment
+    setUserInput(userInput);
+  };
+
+  if (userInput){
+    const yearlyData = []; // array to hold per-year results
+
+    // declare variables for each value
     let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
     const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
     const expectedReturn = +userInput['expected-return'] / 100;
-    const duration = +userInput['duration'];
-
-    // The below code calculates yearly results (total savings, interest etc)
+    const duration = +userInput['duration']; // years, number of rows
+  
+    // for each year in duration, calculate yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
       const yearlyInterest = currentSavings * expectedReturn;
       currentSavings += yearlyInterest + yearlyContribution;
       yearlyData.push({
-        // feel free to change the shape of the data pushed to the array!
         year: i + 1,
         yearlyInterest: yearlyInterest,
         savingsEndOfYear: currentSavings,
         yearlyContribution: yearlyContribution,
       });
     }
-
-    // do something with yearlyData ...
-  };
+  }
 
   return (
     <div>
       <Header></Header>
-      <NewInvestment></NewInvestment>
-      <Investment></Investment>
+      <NewInvestment onSaveInvestmentData={calculateHandler}/>
+      <InvestmentResults items={userInput}/>
     </div>
   )
 }
